278aa9c2e0fdb809e45db5fed8dca33ef1e71fe6
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-12-17 23:13:44 -0800

    all of these files were moved from the root directory to their respective directories

--- syncthing-services.zsh
+++
@@ -1,7 +0,0 @@
-echo "######################################################################################"
-echo "               ${YELLOW}!!  Enabling Syncthing Services !!${NC}                      "
-echo "######################################################################################"
-
-#Start syncthing services
-sudo systemctl enable --now syncthing@jledesma44.service
-sudo systemctl start syncthing@jledesma44.service
