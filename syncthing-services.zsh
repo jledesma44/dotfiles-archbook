7179b27fbeaca254979634bfa6e19e095c24ce9a
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2025-12-17 21:08:17 -0800

    modified synthing-services script to enable and start services

--- syncthing-services.zsh
+++ syncthing-services.zsh
@@ -1,8 +1,7 @@
-
 echo "######################################################################################"
 echo "               ${YELLOW}!!  Enabling Syncthing Services !!${NC}                      "
 echo "######################################################################################"
 
 #Start syncthing services
-sudo systemctl enable --now syncthing@jledesma44.service 
-
+sudo systemctl enable --now syncthing@jledesma44.service
+sudo systemctl start syncthing@jledesma44.service
