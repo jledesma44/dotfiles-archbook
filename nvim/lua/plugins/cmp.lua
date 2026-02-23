964cb7db8ad0af8664ccf3ddc61297341c9c78b2
jledesma44 <49803962+jledesma44@users.noreply.github.com>
2026-02-22 22:33:01 -0800

    added cmp.lua plugin

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "windwp/nvim-autopairs", opts = {} },
+    },
+    opts = function()
+      local cmp = require("cmp")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      -- Insert parentheses after selecting method/function
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+}
